ba1e3ac9513867d59e8e6e2e42fa79372e9ff8f6
abhitanshu singh <abhitanshusingh@gmail.com>
2021-07-29 12:46:24 +0530

    ADDED: MobileCallService class to call the user mobile number

---
+++ CovidVaccineNotifierBackEnd/src/main/java/com/baghel/service/MobileCallService.java
@@ -0,0 +1,11 @@
+package com.baghel.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface MobileCallService {
+
+	public void run() throws Exception;
+
+	public void init();
+}
