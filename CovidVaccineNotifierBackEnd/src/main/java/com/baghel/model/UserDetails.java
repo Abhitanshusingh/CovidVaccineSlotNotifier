410623bce1cac69640b263e8770f4e00190bd742
abhitanshu singh <abhitanshusingh@gmail.com>
2021-07-29 12:30:54 +0530

    ADDED: UserDetails pojo class

---
+++ CovidVaccineNotifierBackEnd/src/main/java/com/baghel/model/UserDetails.java
@@ -0,0 +1,49 @@
+package com.baghel.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_details")
+public class UserDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "mobile_number")
+	private String mobileNumber;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+}
