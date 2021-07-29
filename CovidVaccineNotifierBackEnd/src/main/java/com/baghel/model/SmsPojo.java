c8959ea116e54471050d0afc11a281835ea89192
abhitanshu singh <abhitanshusingh@gmail.com>
2021-07-29 12:30:16 +0530

    ADDED: SmsPojo class

---
+++ CovidVaccineNotifierBackEnd/src/main/java/com/baghel/model/SmsPojo.java
@@ -0,0 +1,27 @@
+package com.baghel.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SmsPojo {
+	
+	private String to;
+	private String message;
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
